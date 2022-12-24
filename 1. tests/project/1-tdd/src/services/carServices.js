d04e0ccbd612ca5e1b76b9c660d73f164c7e75a1
bruno <brunofay1@hotmail.com>
2022-12-23 21:58:02 -0300

    NEW/TRASACTIONS add transactions entity and add tests to cover it

--- 1. tests/project/1-tdd/src/services/carServices.js
+++ 1. tests/project/1-tdd/src/services/carServices.js
@@ -1,8 +1,19 @@
 const BaseRepository = require('../repository/base');
+const Tax = require('../entities/tax');
+const Transaction = require('../entities/transaction');
 
 class CarService {
   constructor({ cars }) {
-    this.carRepository = new BaseRepository({ file: cars })
+    this.carRepository = new BaseRepository({ file: cars });
+    this.taxesBaseOnAge = Tax.taxesBaseOnAge;
+    this.currencyFormat = new Intl.NumberFormat('pt-br', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+  }
+
+  calculateFinalPrice() {
+
   }
 
   getRandomPositionFromArray(list) {
@@ -22,6 +33,34 @@ class CarService {
     return car
   }
 
+  calculateFinalPrice(customer, carCategory, numberOfDays) {
+    const { age } = customer
+    const { price } = carCategory
+    const { tax } = this.taxesBaseOnAge.find(({ from, to }) => age >= from && age <= to)
+    const finalPrice = ((tax * price) * (numberOfDays))
+    const formattedPrice = this.currencyFormat.format(finalPrice)
+
+    return formattedPrice
+  }
+
+  async rent(customer, carCategory, numberOfDays) {
+    const car = await this.getAvailableCar(carCategory)
+    const finalPrice = this.calculateFinalPrice(customer, carCategory, numberOfDays)
+
+    const today = new Date()
+    today.setDate(today.getDate() + numberOfDays)
+
+    const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' }
+    const dueDate = today.toLocaleDateString("pt-br", optionsDate)
+
+    const transaction = new Transaction({
+      car,
+      dueDate,
+      amount: finalPrice,
+      customer
+    })
+    return transaction
+  }
 }
 
 module.exports = CarService
--- 1. tests/project/1-tdd/tests/unitTests/carService.test.js
+++ 1. tests/project/1-tdd/tests/unitTests/carService.test.js
@@ -1,4 +1,5 @@
 const CarService = require('../../src/services/carServices');
+const Transaction = require('../../src/entities/transaction');
 const { describe, it, before, beforeEach, afterEach } = require('mocha')
 const sinon = require('sinon');
 const { join } = require('path');
@@ -49,8 +50,8 @@ describe('CarService Suite tests', () => {
     const result = carService.chooseRandomCar(carCategory)
     const expected = carCategory.carsIds[carIdIndex]
 
-    expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok
     expect(result).to.be.equal(expected)
+    expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok
   })
 
   it('given a car category it should return an available car', async () => {
@@ -74,5 +75,63 @@ describe('CarService Suite tests', () => {
     expect(carService.carRepository.find.calledWithExactly(carMock.id)).to.be.ok
     expect(result).to.be.deep.equal(expected)
 
+  })
+
+  it('given category car, customer age and number Of days it should calculate final amount in R$', () => {
+    const customer = Object.create(mocks.validCustomer)
+    customer.age = 50
+
+    const carCategory = Object.create(mocks.validCarCategory)
+    carCategory.price = 37.6
+
+    const numberOfDays = 5
+
+    /* age:50 -1.3 tax - categoryPrice 37.6
+    37.6 * 1.3 = 48,88 * 5 days = 244.40
+    */
+
+    /* don't depend of external data! stub and mock everything */
+    sandbox.stub(carService, 'taxesBaseOnAge').get(() => [{ from: 40, to: 50, tax: 1.3 }])
+
+    const expected = carService.currencyFormat.format(244.40)
+    const result = carService.calculateFinalPrice(customer, carCategory, numberOfDays)
+
+    expect(result).to.be.equal(expected)
+  })
+
+  it('given a customer and a car category it should return a transaction receipt', async () => {
+    const carMock = mocks.validCar
+    const carCategoryMock = {
+      ...mocks.validCarCategory,
+      price: 37.6,
+      carsIds: [carMock.id]
+    }
+    const customerMock = Object.create(mocks.validCustomer)
+    customerMock.age = 20
+
+    const numberOfDays = 5
+    const dueDate = "10 de novembro de 2020"
+
+    const now = new Date(2020, 10, 5)
+
+    /* stub function new date */
+    sandbox.useFakeTimers(now.getTime())
+
+
+    const expectedAmount = carService.currencyFormat.format(206.80)
+    const result = await carService.rent(
+      customerMock,
+      carCategoryMock,
+      numberOfDays
+    )
+
+    const expected = new Transaction({
+      car: carMock,
+      amount: expectedAmount,
+      dueDate,
+      customer: customerMock
+    })
+
+    expect(result).to.be.deep.equal(expected)
   })
 })
