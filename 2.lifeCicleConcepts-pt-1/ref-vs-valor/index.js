3b5208b7098ac64c09ecbc1a84614aeb409142a7
bruno <brunofay1@hotmail.com>
2022-12-28 22:01:32 -0300

    NEW/LifeCicleConcepts start second module in js expert course

---
+++ 2.lifeCicleConcepts-pt-1/ref-vs-valor/index.js
@@ -0,0 +1,19 @@
+const { deepStrictEqual } = require('assert')
+
+let counter = 0
+let counter2 = counter
+counter2++
+
+const item = { counter: 0 }
+const item2 = item
+
+/* primitive type make a copy in memory */
+deepStrictEqual(counter, 0)
+deepStrictEqual(counter2, 1)
+
+/* reference type,copy the memory address and points to the same place */
+
+item2.counter++
+deepStrictEqual(item, { counter: 1 })
+item2.counter++
+deepStrictEqual(item2, { counter: 2 })
